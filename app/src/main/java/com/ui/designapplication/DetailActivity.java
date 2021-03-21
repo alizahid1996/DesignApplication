ce708a208b86df91d6d1aeb1375bd54fbc6d932e
zia1122 <ziarehman.cs@gmail.com>
2021-03-22 02:04:43 +0500

    Commit 1 Zia

--- app/src/main/java/com/ui/designapplication/DetailActivity.java
+++ app/src/main/java/com/ui/designapplication/DetailActivity.java
@@ -1,10 +1,13 @@
 package com.ui.designapplication;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -12,6 +15,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
@@ -28,6 +32,7 @@ import org.json.JSONTokener;
 import java.io.BufferedReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.NumberFormat;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -36,9 +41,9 @@ public class DetailActivity extends AppCompatActivity {
 
     ActivityDetailBinding binding;
     private RequestQueue mQueue;
-    private static final String URL = "https://godiapi.azurewebsites.net/api/cards/getParkById/600764a194361a4b14bf2a35";
     String id;
     String name;
+    String Description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,38 +56,65 @@ public class DetailActivity extends AppCompatActivity {
             finish();
         });
 
-        
 
         Toast.makeText(this, "" + name, Toast.LENGTH_SHORT).show();
 
+         DataFetching();
+        //NewDataFetching();
+    }
 
 
-
-
-   /* private void fetchData(){
-
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+    private void DataFetching() {
+        String url = "https://godiapi.azurewebsites.net/api/cards/getParkById/600764a194361a4b14bf2a35";
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
+                        //Fetching data from API and storing into string
                         try {
-                            response = response.getJSONObject("id");
+                            id = response.getString("id");
+                            name = response.getString("name");
+                            Description = response.getString("description");
+                            //str_death = response.getString("deceased");
+                            Handler delayToshowProgress = new Handler();
+                            delayToshowProgress.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+
+                                    binding.tv2.setText(name);
+                                    binding.Description.setText(Description);
+                                   /* binding.affected.setText(NumberFormat.getInstance().format(Integer.parseInt(str_confirmed)));
+                                    int_active_new = Integer.parseInt(str_confirmed) - (Integer.parseInt(str_recovered) + Integer.parseInt(str_death));
+                                    binding.active.setText("+" + NumberFormat.getInstance().format(int_active_new));
+
+                                    binding.recovered.setText(NumberFormat.getInstance().format(Integer.parseInt(str_recovered)));
+
+                                    binding.death.setText(NumberFormat.getInstance().format(Integer.parseInt(str_death)));
+
+                                    binding.trackingPiechart.invalidate();
+                                    setData(Float.parseFloat(str_confirmed),Float.parseFloat(str_recovered),Float.parseFloat(str_death));
+*/
+
+                                }
+                            }, 1000);
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
-
-
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
+                Toast.makeText(DetailActivity.this, error.toString(), Toast.LENGTH_LONG).show();
             }
         });
-        mQueue.add(request);
-    }*/
-    }
 
+        requestQueue.add(jsonObjectRequest);
+    }
 
 }
 
