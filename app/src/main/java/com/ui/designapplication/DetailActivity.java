f2075e1c62fd372516ac55220b824a29405d7035
Ali Zahid <ali.zahid050@gmail.com>
2021-03-23 14:13:13 +0500

    commit 104 ali

--- app/src/main/java/com/ui/designapplication/DetailActivity.java
+++ app/src/main/java/com/ui/designapplication/DetailActivity.java
@@ -105,6 +105,7 @@ public class DetailActivity extends AppCompatActivity implements OnMapReadyCallb
             }
         });
 
+        //Back Button
         binding.backButton.setOnClickListener(v -> {
             finish();
         });
