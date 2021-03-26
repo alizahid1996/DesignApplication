62dd004aafdf9213991d7b87c60350305501d262
zia1122 <ziarehman.cs@gmail.com>
2021-03-26 17:38:50 +0500

    Commit 8 Zia

--- app/src/main/java/com/ui/designapplication/DetailActivity.java
+++ app/src/main/java/com/ui/designapplication/DetailActivity.java
@@ -60,7 +60,9 @@ import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -77,50 +79,63 @@ public class DetailActivity extends AppCompatActivity implements OnMapReadyCallb
     private static final String TAG = "DetailActivity";
     ActivityDetailBinding binding;
     private RequestQueue mQueue;
-    String id;
-    String name;
-    String Description;
-    String decription2;
-    String lat, lng;
+    Uri imageData;
+
+    public static final int CAMERA_PERMISSION_CODE = 1;
+    public static final int CAMERA_IMAGE_CODE = 10;
+    public static final int GALLERY_IMAGE_CODE = 100;
+
     public static final int DRIVER_CAMERA_IMAGE_CODE = 2;
     private ProgressDialog progressDialog;
     private double latitude = 38.282771;
     private double longtitude = -77.647043;
     private ArrayList<String> mImageUrls = new ArrayList<>();
-    String imageEncoded;
-    String chkpath = null;
-    private String mCurrentPhotoPath;
     ArrayList<Data_Model> arrayList;
-    MyAdapter m;
-    int i = 0;
-    static final int REQUEST_TAKE_PHOTO = 1;
     private int SELECT_FILE = 2;
-    private String userChoosenTask;
-    private Uri fileUri; // file url to store image/video
 
+    String id="";
+    String name="";
 
-    /*String[] afterSplitLat = lat.split(" ");
-    String[] afterSplitLng = lng.split(" ");
+    String Description="";
+    String decription2="";
+    String address="";
+    String city="";
+    String streetAddress="";
+    String state="";
+    String zipcode="";
 
-    double latitude = Double.parseDouble(afterSplitLat[0]);
-
-    double longitude = Double.parseDouble(afterSplitLng[1]);*/
-
+    String lattitude="";
+    String longitude="";
+    String imageUrl="";
+    String reviews="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityDetailBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        arrayList = new ArrayList<>();
-        m = new MyAdapter();
-        Log.d("oncreate", "set adapter");
-        binding.recyclerView.setAdapter(m);
-        ViewGroup.LayoutParams params = binding.recyclerView.getLayoutParams();
-        params.height = 200;
-        binding.recyclerView.setLayoutParams(params);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        binding.recyclerView.setLayoutManager(linearLayoutManager);
+
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null){
+            id = bundle.getString("id");
+            name = bundle.getString("name");
+            Description = bundle.getString("Description");
+            decription2 = bundle.getString("decription2");
+            city = bundle.getString("city");
+            streetAddress = bundle.getString("streetAddress");
+            state = bundle.getString("state");
+            zipcode = bundle.getString("zipcode");
+            lattitude= bundle.getString("latitude");
+            longitude = bundle.getString("longitude");
+            imageUrl = bundle.getString("imageUrl");
+            reviews = bundle.getString("reviews");
+
+        }
+
+      address = streetAddress+""+city+""+state+""+zipcode;
+         binding.tv2.setText(name);
+        binding.tv3.setText(address);
+
 
         //Back Button
         binding.backButton.setOnClickListener(v -> {
@@ -130,7 +145,7 @@ public class DetailActivity extends AppCompatActivity implements OnMapReadyCallb
         binding.pickPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                selectImage();
+                choseImage();
 
             }
         });
@@ -140,48 +155,14 @@ public class DetailActivity extends AppCompatActivity implements OnMapReadyCallb
         mapFragment.getMapAsync(this);
 
         ShowDialog(DetailActivity.this);
-        DataFetching();
 
     }
 
-    public void takepicture() {
-        Log.d("Cameraclick", "takepicture");
-
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
 
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
-                        Uri.fromFile(photoFile));
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-            }
-        }
-    }
 
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
 
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = image.getAbsolutePath();
-        return image;
 
-    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -193,56 +174,8 @@ public class DetailActivity extends AppCompatActivity implements OnMapReadyCallb
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
-    private void DataFetching() {
-        String url = "https://godiapi.azurewebsites.net/api/cards/getParkById/600764a194361a4b14bf2a35";
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                Request.Method.GET,
-                url,
-                null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        //Fetching data from API and storing into string
-                        try {
-                            id = response.getString("id");
-                            name = response.getString("name");
-                            Description = response.getString("description");
-                            decription2 = response.getString("directions");
-                            lng = response.getString("long");
-                            lat = response.getString("lat");
-                            Log.d("name", "lat" + lat);
 
-                            DismissDialog();
-                            JSONArray arr = response.getJSONArray("weatherForecast");
-                            for (int i = 0; i < arr.length(); i++) {
-                                JSONObject jobj = arr.getJSONObject(i);
-                            }
-                            Handler delayToshowProgress = new Handler();
-                            delayToshowProgress.postDelayed(new Runnable() {
-                                @Override
-                                public void run() {
 
-                                    binding.tv2.setText(name);
-                                    binding.Description.setText(Description);
-                                    binding.descriptionDetail.setText(decription2);
-                                    DismissDialog();
-                                }
-                            }, 1);
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(DetailActivity.this, error.toString(), Toast.LENGTH_LONG).show();
-            }
-        });
-
-        requestQueue.add(jsonObjectRequest);
-    }
 
     public void ShowDialog(Context context) {
         //setting up progress dialog
@@ -257,193 +190,59 @@ public class DetailActivity extends AppCompatActivity implements OnMapReadyCallb
         progressDialog.dismiss();
     }
 
+
+
+
+
+    private void choseImage() {
+        Intent imageIntent = new Intent();
+        imageIntent.setType("image/*");
+        imageIntent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(imageIntent, "Select Picture"), GALLERY_IMAGE_CODE);
+
+    }
+
+    //On Activity Result
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        if (requestCode == GALLERY_IMAGE_CODE && resultCode == RESULT_OK) {
+            imageData = imageReturnedIntent.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageData);
+                final byte[] inputData = getImageBytes(bitmap);
+                binding.image.setImageBitmap(bitmap);
 
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            if (requestCode == REQUEST_TAKE_PHOTO) {
-
-                // Save Image To Gallery
-                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-                File f = new File(mCurrentPhotoPath);
-                Uri contentUri = Uri.fromFile(f);
-                mediaScanIntent.setData(contentUri);
-                this.sendBroadcast(mediaScanIntent);
-                String clickpath = mCurrentPhotoPath;
-                Data_Model data_model = new Data_Model();
-                data_model.setImage(clickpath);
-                arrayList.add(data_model);
-                binding.recyclerView.setAdapter(m);
-                m.notifyDataSetChanged();
-
-
-
-            } else if (requestCode == SELECT_FILE) {
-                Log.d("gallery", "checkpointd");
-                onSelectFromGalleryResult(data);
-            }
-        }
-
-    }
-
-    class MyAdapter extends RecyclerView.Adapter<MyAdapter.Myviewholder> {
-
-        @Override
-        public Myviewholder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View v = getLayoutInflater().inflate(R.layout.item, parent, false);
-            Myviewholder myviewholder = new Myviewholder(v);
-            Log.d("myactivty ", "oncreateViewHolder");
-
-            return myviewholder;
-        }
-
-        @Override
-        public void onBindViewHolder(Myviewholder holder, final int position) {
-            final Data_Model m = arrayList.get(position);
-            Log.d(" myactivty", "onBindviewholder" + position);
-            //holder.imageView.setVisibility(View.VISIBLE);
-            // bimatp factory
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            // downsizing image as it throws OutOfMemory Exception for larger
-            // images
-
-            options.inSampleSize = 20;
-            final Bitmap bitmap = BitmapFactory.decodeFile(m.getImage(), options);
-            holder.imageView.setImageBitmap(bitmap);
-            holder.imageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(getApplicationContext(), ImageDisplay.class);
-                    String chkpath = m.getImage();
-                    intent.putExtra("path", chkpath);
-                    Log.d("intent", "new activity");
-                    startActivity(intent);
-                }
-            });
-            holder.imageView.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View view) {
-
-                    final int pst = position;
-                    DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            switch (which) {
-                                case DialogInterface.BUTTON_POSITIVE:
-                                    arrayList.remove(position);
-                                    notifyItemRemoved(position);
-                                    notifyItemRangeChanged(position, arrayList.size());
-                                    // Yes button clicked
-                                    Toast.makeText(DetailActivity.this, "Yes Clicked",
-                                            Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case DialogInterface.BUTTON_NEGATIVE:
-                                    // No button clicked
-                                    // do nothing
-                                    Toast.makeText(DetailActivity.this, "No Clicked",
-                                            Toast.LENGTH_LONG).show();
-                                    break;
-                            }
-                        }
-                    };
-
-                    AlertDialog.Builder builder = new AlertDialog.Builder(DetailActivity.this);
-                    builder.setMessage("Are you sure want to Delete ?")
-                            .setPositiveButton("Yes", dialogClickListener)
-                            .setNegativeButton("No", dialogClickListener).show();
-
-                    return true;
-                }
-            });
-
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return arrayList.size();
-        }
-
-        public class Myviewholder extends RecyclerView.ViewHolder {
-            public ImageView imageView;
-
-            public Myviewholder(View itemView) {
-                super(itemView);
-                imageView = (ImageView) itemView.findViewById(R.id.image);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
 
-    private void onSelectFromGalleryResult(Intent data) {
-
-        Uri selectedImageUri = data.getData();
-        String[] projection = {MediaStore.Images.Media.DATA};
-        Cursor cursor = managedQuery(selectedImageUri, projection, null, null, null);
-        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-        cursor.moveToFirst();
-        String path = cursor.getString(column_index);
-        Data_Model data_model = new Data_Model();
-        data_model.setImage(path);
-        arrayList.add(data_model);
-        binding.recyclerView.setAdapter(m);
-        m.notifyDataSetChanged();
-        // Toast.makeText(this, "" + path, Toast.LENGTH_LONG).show();
-    }
-
-    private void selectImage() {
-
-        final CharSequence[] items = {"Take Photo", "Choose from Library",
-                "Cancel"};
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(DetailActivity.this);
-        builder.setTitle("Add Photo!");
-        builder.setItems(items, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int item) {
-                boolean result = true;
-
-                if (items[item].equals("Take Photo")) {
-                    userChoosenTask = "Take Photo";
-                    if (result)
-                        takepicture();
-
-                } else if (items[item].equals("Choose from Library")) {
-                    userChoosenTask = "Choose from Library";
-                    if (result)
-                        galleryIntent();
-
-
-                } else if (items[item].equals("Cancel")) {
-                    dialog.dismiss();
-                }
-            }
-        });
-        builder.show();
-    }
-
-    public void galleryIntent() {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        Log.d("gallery", "checkpointA");
-        startActivityForResult(Intent.createChooser(intent, "Select File"), SELECT_FILE);
-        Log.d("gallery", "checkpointB");
-    }
+    //On Request Permission Result
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_TAKE_PHOTO) {
+        if (requestCode == CAMERA_PERMISSION_CODE) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(DetailActivity.this, "Camera permission granted", Toast.LENGTH_LONG).show();
                 Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, DRIVER_CAMERA_IMAGE_CODE);
+                startActivityForResult(cameraIntent, CAMERA_IMAGE_CODE);
             } else {
                 Toast.makeText(DetailActivity.this, "Camera permission denied", Toast.LENGTH_LONG).show();
             }
         }
-// Capture Image From Camera End
+    }
 
+
+
+    public byte[] getImageBytes(Bitmap inImage) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+        byte[] imageBytes = outputStream.toByteArray();
+        return imageBytes;
     }
 
 
