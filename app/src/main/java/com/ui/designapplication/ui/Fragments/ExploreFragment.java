29ffcd2323095c47e09dead352563c289836fe05
Ali Zahid <ali.zahid050@gmail.com>
2021-03-30 02:23:23 +0500

    commit 108 ali

--- app/src/main/java/com/ui/designapplication/ui/Fragments/ExploreFragment.java
+++ app/src/main/java/com/ui/designapplication/ui/Fragments/ExploreFragment.java
@@ -1,11 +1,15 @@
 package com.ui.designapplication.ui.Fragments;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 
@@ -17,6 +21,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,12 +32,12 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.internal.ConnectionCallbacks;
 import com.google.android.gms.common.api.internal.OnConnectionFailedListener;
-import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.ui.designapplication.Adapter.ParksAdapter;
 import com.ui.designapplication.Models.NearbyCards;
 import com.ui.designapplication.Models.ServerResponse;
+import com.ui.designapplication.MyLocationListener;
 import com.ui.designapplication.R;
 import com.ui.designapplication.databinding.FragmentExploreBinding;
 import com.ui.designapplication.network.ApiClient;
@@ -59,25 +64,22 @@ public class ExploreFragment extends Fragment {
     String state;
     String zipcode;
     ProgressDialog progressDialog;
-    String type, phoneNumber, hoursOfOperation, website, email, longitude, latitude, stayLimit;
-    String acres, tags, tagCount, contentImage, file, fileString, lastUpdated, origin, approved;
-    String status, likeCount, imageUrl, reviews, photos, weatherForecast;
-    String lat ;
-    String lng ;
-    String p;
+
+
     LocationManager locationManager;
     private ApiInterface apiInterface;
     private static final int REQUEST_Location = 1;
-    private GoogleApiClient mGoogleApiClient;
-    private LocationRequest mLocationRequest;
     private double currentLatitude;
     private double currentLongitude;
-
     private ParksAdapter adapter;
-
     private List<NearbyCards> models;
-
     FragmentExploreBinding binding;
+
+    private static final int REQUEST_LOCATION = 1;
+    LocationManager locManager;
+
+    /*These are the variables for Lat and Long*/
+    String currLat, currLong;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -92,8 +94,31 @@ public class ExploreFragment extends Fragment {
 
 
         //Add Permission
-        ActivityCompat.requestPermissions(getActivity(),new String[]{
-                Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_Location);
+        ActivityCompat.requestPermissions(getActivity(), new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_Location);
+
+        //Get Location
+        binding.getLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+                // Check if Gps is ON or not
+                if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+                {
+                    turnOnGps();
+                }
+                else
+                {
+                    //Gps is laready ON
+                    getCurrentLocation();
+                }
+            }
+        });
+
+        /*Log.d("lat", currLat);
+        Log.d("long", currLong);*/
+
 
 
 
@@ -102,6 +127,77 @@ public class ExploreFragment extends Fragment {
         ShowDialog(getActivity());
         return binding.getRoot();
     }
+
+    private void getCurrentLocation() {
+        //Check Permission
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            //Add Permission
+            ActivityCompat.requestPermissions(getActivity(), new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_Location);
+        }
+        else
+        {
+            Location locGps = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            Location locNet = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            Location locPassive =locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+
+            if (locGps != null)
+            {
+                currentLatitude = locGps.getLatitude();
+                currentLongitude = locGps.getLongitude();
+                currLat = String.valueOf(currentLatitude);
+                currLong = String.valueOf(currentLongitude);
+                binding.lat.setText(currLat);
+                binding.longi.setText(currLong);
+
+            } else if (locNet != null)
+            {
+                currentLatitude = locNet.getLatitude();
+                currentLongitude = locNet.getLongitude();
+                currLat = String.valueOf(currentLatitude);
+                currLong = String.valueOf(currentLongitude);
+                binding.lat.setText(currLat);
+                binding.longi.setText(currLong);
+            }
+            else if (locPassive != null)
+            {
+                currentLatitude = locPassive.getLatitude();
+                currentLongitude = locPassive.getLongitude();
+                currLat = String.valueOf(currentLatitude);
+                currLong = String.valueOf(currentLongitude);
+
+                binding.lat.setText(currLat);
+                binding.longi.setText(currLong);
+            }
+            else
+            {
+                Toast.makeText(getActivity(), "Can't get your location", Toast.LENGTH_SHORT).show();
+            }
+
+
+        }
+    }
+
+    private void turnOnGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("Enable Gps").setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            }
+        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
 
     private void getServerResponse() {
 
@@ -291,8 +387,6 @@ public class ExploreFragment extends Fragment {
     }*/
 
 
-
-
     public void ShowDialog(Context context) {
         //setting up progress dialog
         progressDialog = new ProgressDialog(context);
@@ -305,4 +399,32 @@ public class ExploreFragment extends Fragment {
     public void DismissDialog() {
         progressDialog.dismiss();
     }
+
+    private Location getLastBestLocation() {
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+        }
+        Location locationGps = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        Location locationNet = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+        long GPSLocationTime = 0;
+        if (null != locationGps) {
+            GPSLocationTime = locationGps.getTime();
+        }
+
+        long NetLocationTime = 0;
+
+        if (null != locationNet) {
+            NetLocationTime = locationNet.getTime();
+        }
+
+        if (0 < GPSLocationTime - NetLocationTime) {
+            return locationGps;
+        } else {
+            return locationNet;
+        }
+
+    }
+
+
 }
